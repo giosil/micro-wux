a2656c75f96a95c8aa5661377b95a4dc224f6171
giosil <giorgio.silvestris@gmail.com>
2025-10-20 12:11:41 +0200

    update

--- micro-wux-app/ts/app/app.ts
+++ micro-wux-app/ts/app/app.ts
@@ -1,9 +1,9 @@
 ﻿namespace APP {
 
-    export class Main extends WUX.WComponent {
-        protected render() {
-            return '<div>Hello World!</div>';
-        }
-    }
+	export class Main extends WUX.WComponent {
+		protected render() {
+			return '<div>Hello World!</div>';
+		}
+	}
 
 }
